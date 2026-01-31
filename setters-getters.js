2c29ca1554877f45c972710ee4a0919ab96683bd
Miki <mikonodi33@gmail.com>
2026-01-31 12:52:53 +0100

    Added a setters and getters js cheat sheet

---
+++ setters-getters.js
@@ -0,0 +1,37 @@
+class Rectangle {
+  constructor(width, height) {
+    this.width = width;
+    this.height = height;
+  }
+
+  set width(newWidth) {
+    if (newWidth > 0) {
+      this._width = newWidth;
+    } else {
+      console.error("Width must be a positive number");
+    }
+  }
+  set height(newHeight) {
+    if (newHeight > 0) {
+      this._height = newHeight;
+    } else {
+      console.error("Height must be a positive number");
+    }
+  }
+
+  get width() {
+    return this._width;
+  }
+  get height() {
+    return this._height;
+  }
+  get area() {
+    return this._width * this._height;
+  }
+}
+
+const rectangle = new Rectangle(3, 4);
+
+console.log(rectangle.width);
+console.log(rectangle.height);
+console.log(rectangle.area);
